098dfa7d171a84244b3ea8d54bdfeeb211b77dc3
seonoleo <mscomy1@gmail.com>
2025-11-06 15:29:58 +0900

    10.3

--- main.c
+++ main.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
+void main(int argc, char *argv[])
 {
-  int i=0;
-  char str[4];
+  char src[]="The worst things to eat before you sleep";
+  char dst[100];
   
-  str[0]='a';
-  str[1]='b';
-  str[2]='c';
-  str[3]='\0';
-
+  strcpy(dst,src);
   
-  while (str[i]!='\0')
-  {
-   printf("%c",str[i]);
-   i++;     
-}
+  printf("copied string: %s\n", dst);
   
   
   system("PAUSE");	
